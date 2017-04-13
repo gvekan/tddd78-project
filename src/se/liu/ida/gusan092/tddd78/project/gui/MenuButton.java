639585624f6d5eaa4903d2ac9dff26c47a8c08df
Gustav Andersson <gusan092@student.liu.se>
2017-04-13 21:26:45 +0200

    Började skriva lite på gui

---
+++ src/se/liu/ida/gusan092/tddd78/project/gui/MenuButton.java
@@ -0,0 +1,24 @@
+package se.liu.ida.gusan092.tddd78.project.gui;
+
+import javax.swing.JComponent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MenuButton extends JComponent implements MouseListener
+{
+
+    @Override public void mouseClicked(final MouseEvent e) {
+    }
+
+    @Override public void mousePressed(final MouseEvent e) {
+    }
+
+    @Override public void mouseReleased(final MouseEvent e) {
+    }
+
+    @Override public void mouseEntered(final MouseEvent e) {
+    }
+
+    @Override public void mouseExited(final MouseEvent e) {
+    }
+}
